a4517aff13c2eccf2a8f2d5c10e23a73b973d7fa
Angelina Butenko <angelinaa2712@gmail.com>
2023-06-26 18:44:35 +0300

    Working with Services and Dependency Injection

--- HwCityInfo.API/Controllers/CitiesController.cs
+++ HwCityInfo.API/Controllers/CitiesController.cs
@@ -1,4 +1,5 @@
 ﻿using HwCityInfo.API.Models;
+using HwCityInfo.API.Servises;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HwCityInfo.API.Controllers
@@ -7,16 +8,23 @@ namespace HwCityInfo.API.Controllers
     [Route("api/cities")]
     public class CitiesController : ControllerBase
     {
+        private readonly CitiesDataStore _citiesDataStore;
+
+        public CitiesController(CitiesDataStore citiesDataStore) 
+        {
+            _citiesDataStore = citiesDataStore ?? throw new ArgumentNullException(nameof(citiesDataStore));
+        }
+
         [HttpGet]
         public ActionResult<IEnumerable<CityDto>> GetCities()
         {
-            return Ok(CitiesDataStore.Current.Cities);
+            return Ok(_citiesDataStore.Cities);
         }
 
         [HttpGet("{id}")]
         public ActionResult<CityDto> GetCity(int id) 
         { 
-            var cityToReturn = CitiesDataStore.Current.Cities
+            var cityToReturn = _citiesDataStore.Cities
                 .FirstOrDefault(x => x.Id == id);
 
             if (cityToReturn == null)
