08c1e86341679f5b15610677b46aee92425c7ef5
Angelina Butenko <angelinaa2712@gmail.com>
2023-08-23 20:47:25 +0300

    Versioning and Documenting

--- HwCityInfo.API/Controllers/PointsOfInterestController.cs
+++ HwCityInfo.API/Controllers/PointsOfInterestController.cs
@@ -8,8 +8,9 @@ using System.Data;
 
 namespace HwCityInfo.API.Controllers;
 
-[Route("api/cities/{cityId}/pointsofinterest")]
+[Route("api/v{version:apiVersion}/cities/{cityId}/pointsofinterest")]
 [Authorize(Policy = "MustBeFromAntwerp")]
+[ApiVersion("2.0")]
 [ApiController]
 public class PointsOfInterestController : ControllerBase
 {
@@ -36,12 +37,12 @@ public class PointsOfInterestController : ControllerBase
     [HttpGet]
     public async Task <ActionResult<IEnumerable<PointOfInterestDto>>> GetPointsOfInterst(int cityId)
     {
-        var cityName = User.Claims.FirstOrDefault(c => c.Type == "city")?.Value;
+        //var cityName = User.Claims.FirstOrDefault(c => c.Type == "city")?.Value;
 
-        if (!await _cityInfoRepository.CityNameMatchesCityId(cityName, cityId))
-        {
-            return Forbid();
-        }
+        //if (!await _cityInfoRepository.CityNameMatchesCityId(cityName, cityId))
+        //{
+        //    return Forbid();
+        //}
 
         if (!await _cityInfoRepository.CityExistsAsync(cityId))
         {
